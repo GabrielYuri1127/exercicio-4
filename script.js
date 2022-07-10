b27601e8a2b372864fd136c0896ef1f6e25e0345
GabrielYuri1127 <gabrielcavalcante1127@gmail.com>
2022-07-10 10:50:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//1 - crie uma função que exiba uma mensagem no console
+function eu(){console.log(`olá mundo`)}
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function vnw(Gabriel){console.log(`meu nome é ${Gabriel}`)}
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+function olá (nome, idade, musica){console.log(`meu nome é ${nome}`,`minha idade é ${idade}`,`minha música é ${musica}`)}
+olá(`Gabriel`, 17, `pop`)
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function film(filme,musica){console.log(`meu filme é ${filme}`, `minha música é ${musica}`)}
+film(`Harry potter`, `bad romance`)
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+function math(number){
+    return 3* number
+}console.log(math(3*number))
